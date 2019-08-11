b94c393cc960e820cc3e73ee6d02f6a29f621846
Tansu Dasli <tansudasli@gmail.com>
2019-08-11 22:07:27 +0300

    Initialize - /nav impl.

--- semantics/src/app/app-routing.module.ts
+++ semantics/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent,  data: { title: 'Index' } },
+  { path: 'nav', component: NavComponent,  data: { title: 'Navigator' }},
+  // { path: '', redirectTo: 'index', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
