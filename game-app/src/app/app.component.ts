8d25f68278540cf2ff32359e6eec54011d9fffdf
Tansu Dasli <tansudasli@gmail.com>
2019-10-14 19:50:02 +0300

    Initialize - game-app w/ custom event

--- game-app/src/app/app.component.ts
+++ game-app/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'game-app';
+
+  onIntervalFired($event: number) {
+    console.log($event);
+  }
 }
--- game-app/src/app/app.module.ts
+++ game-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
