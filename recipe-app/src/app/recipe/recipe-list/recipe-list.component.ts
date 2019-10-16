70f2ee973631d89af28460d0177e5c4cdd0424fa
Tansu Dasli <tansudasli@gmail.com>
2019-10-16 11:46:50 +0300

    Initialize - recipe-app make-up w/ more data + ingredients

--- recipe-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -21,7 +21,21 @@ export class RecipeListComponent implements OnInit {
     // tslint:disable-next-line:max-line-length
     new Recipe('just boiled eggs', 'made w/ eggs', 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Scrambled-with-Milk-930x620.jpg'),
     // tslint:disable-next-line:max-line-length
-    new Recipe('menemen - turkish style', 'made w/ eggs and bacon', 'https://www.seriouseats.com/images/2014/09/20140923-menemen-recipe-14.jpg')
+    new Recipe('menemen - turkish style', 'made w/ eggs and bacon', 'https://www.seriouseats.com/images/2014/09/20140923-menemen-recipe-14.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('panncake - vegi', 'pancake made w/ vegetables', 'https://www.superhealthykids.com/wp-content/uploads/uploads/files/10735/large/Veggie%20Pancakes%20for%20Kids.png'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('shredded salad - vegi', 'zucchini filled with immune fighting Vitamin C made w/ vegetables', 'https://www.superhealthykids.com/wp-content/uploads/uploads/files/10730/large/shredded%20zucchini.png'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('shredded carrot - vegi', 'shredded finely carrots w/ vegetables', 'https://www.superhealthykids.com/wp-content/uploads/uploads/files/10731/large/shredded%20carrots.png'),
+    new Recipe('corn',
+           'corn that includes energizing Vitamin B',
+           'https://www.superhealthykids.com/wp-content/uploads/uploads/files/10732/large/corn.png',
+          ['2 cup zucchini', '2 cup grated carrot', '1 cup corn, canned',
+                     '1 large egg', '1/8 cup yogurt, plain', '1/2 teaspoon salt',
+                     '1/8 teaspoon black pepper, ground', '1/2 cup flour, whole wheat', '1/2 cup cornmeal, yellow',
+                     '2 teaspoon baking powder', '1/2 cup cheddar cheese, shredded', '1 tablespoon olive oil'])
+
   ];
 
   constructor() { }
--- recipe-app/src/app/recipe/recipe.model.ts
+++ recipe-app/src/app/recipe/recipe.model.ts
@@ -3,10 +3,12 @@ export class Recipe {
   name: string;
   description: string;
   imagePath: string;
+  ingredients: string[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredients?: string[] ) {
       this.name = name;
       this.description = description;
       this.imagePath = imagePath;
+      this.ingredients = ingredients;
   }
 }
