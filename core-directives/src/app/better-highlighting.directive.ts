9a8ea47e5a3678f6c3a9004a500a07c4ce9219d9
Tansu Dasli <tansudasli@gmail.com>
2019-10-17 11:05:25 +0300

    Initialize - core-directives w/ easy dynamic DOM manipulation w/ HostListener & HostBinding and custom property binding !

--- core-directives/src/app/better-highlighting.directive.ts
+++ core-directives/src/app/better-highlighting.directive.ts
@@ -1,31 +1,41 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlighting]'
 })
 export class BetterHighlightingDirective implements OnInit {
-  // backgroundColor is the name in DOM. which does not know background-color !
-  @HostBinding('style.backgroundColor')
-  backgroundColor = 'transparent';
+  // custom property binding to change directive prop
+  @Input() defaultBackgroundColor = 'gold';
+  @Input() defaultColor = 'black';
 
-  @HostBinding('style.color')
-  color = 'black';
+  @Input() highlightBackgroundColor =  '#4a4a4a';
+  @Input() highlightColor =  '#fafafa';
 
+  /**
+   * directive w/ HostBinding to attach DOM
+   * backgroundColor is the name in DOM. which does not know background-color !
+   */
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.color') color: string;
+
+  // sets automatically this.elementRef = elementRef; and creates in Memory
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#4a4a4a');
     // this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#fafafa');
+
+    this.color = this.defaultColor;
+    this.backgroundColor = this.defaultBackgroundColor;
   }
 
   @HostListener('mouseenter')
   onMouseEnter(eventData: Event) {
-
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#4a4a4a');
     // this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#fafafa');
 
-    this.backgroundColor = '#4a4a4a';
-    this.color = '#fafafa';
+    this.backgroundColor = this.highlightBackgroundColor;
+    this.color = this.highlightColor;
   }
 
   @HostListener('mouseleave')
@@ -33,7 +43,7 @@ export class BetterHighlightingDirective implements OnInit {
     // this.elementRef.nativeElement.style.backgroundColor = 'gold';
     // this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#ffffff');
 
-    this.backgroundColor = 'gold';
-    this.color = '#000';
+    this.backgroundColor = this.defaultBackgroundColor;
+    this.color = this.defaultColor;
   }
 }
