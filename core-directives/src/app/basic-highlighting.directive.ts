d9efd5e1b3bd31ecf5e6f37817fa54b23d3e0daf
Tansu Dasli <tansudasli@gmail.com>
2019-10-17 10:31:06 +0300

    Initialize - core-directives w/ dynamic DOM manipulation w/ HostListener & Renderer2

--- core-directives/src/app/basic-highlighting.directive.ts
+++ core-directives/src/app/basic-highlighting.directive.ts
@@ -12,9 +12,12 @@ export class BasicHighlightingDirective implements OnInit {
   /**
    * accessing element
    * 1st way : this.elementRef.nativeElement.style.backgroundColor = 'gold';
-   * 2nd way : use Renderer2 class (better way)!
+   * 2nd way : use Renderer2 class (better way but not interactive
+   * 3rd way : HostListener + Renderer2 class for interactive DOM manipulation such as mouserover events
    */
   ngOnInit() {
     this.elementRef.nativeElement.style.backgroundColor = 'gold';
   }
+
+
 }
--- core-directives/src/app/better-highlighting.directive.ts
+++ core-directives/src/app/better-highlighting.directive.ts
@@ -1,14 +1,27 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlighting]'
 })
-export class BetterHighlightingDirective implements OnInit{
+export class BetterHighlightingDirective implements OnInit {
 
-  constructor(private elementRef: ElementRef ,private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#4a4a4a');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#fafafa');
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter(eventData: Event) {
+
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#4a4a4a');
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#fafafa');
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave(eventData: Event) {
+    this.elementRef.nativeElement.style.backgroundColor = 'gold';
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#ffffff');
   }
 }
