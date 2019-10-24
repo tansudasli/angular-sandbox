9ad19e4026393744358403e2f6817f3ef7be9390
Tansu Dasli <tansudasli@gmail.com>
2019-10-24 19:51:01 +0300

    Standard rxjs/ interval() and operators (map, filter ..)

--- core-observables/src/app/app.component.ts
+++ core-observables/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {interval, Observable, Observer, Subscription} from 'rxjs';
-import {take} from 'rxjs/operators';
+import {filter, map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,41 +15,45 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    // operators
-    // interval(1000)
-    //   .pipe(take(50))
-    //   .subscribe((value) => {
-    //     console.log(value);
-    //   });
+    // observer w/ operators
+    interval(1000)
+      .pipe(
+        take(50),
+        filter((value) => value % 2 === 0),
+        map(value => 2 ** value)
+      ).subscribe((value) => {
+        console.log(value);
+      });
 
-    // standard observer w/ interval()
-    // this.subscription = interval(1000).subscribe((value) => { console.log(value); },
-    //                                                    (error) => { console.log(error); });
+    // // standard observer w/ interval()
+    // this.subscription = interval(1000).subscribe(
+    //                          (value) => { console.log(value); },
+    //                         (error) => { console.log(error); });
 
-    // custom observer creation = of interval
-    const customInterval = new Observable((observer) => {
-      let counter = 0;
-
-      setInterval(() => {
-
-        // completion (not meaningful)
-        if (counter === 10) {
-          observer.complete();
-        }
-
-        // error management
-        if (counter === 15) {
-          observer.error(new Error('no counter can be bigger then 10 :)'));
-        }
-
-        observer.next(counter++);
-        }, 1000);
-    });
-
-    // accessing custom observer
-    customInterval.subscribe((value) => console.log(value),
-                            (error) => console.log(error),
-                         () => console.log('completed'));
+    // // custom observer creation = of interval
+    // const customInterval = new Observable((observer) => {
+    //   let counter = 0;
+    //
+    //   setInterval(() => {
+    //
+    //     // completion (not meaningful)
+    //     if (counter === 10) {
+    //       observer.complete();
+    //     }
+    //
+    //     // error management
+    //     if (counter === 15) {
+    //       observer.error(new Error('no counter can be bigger then 10 :)'));
+    //     }
+    //
+    //     observer.next(counter++);
+    //     }, 1000);
+    // });
+    //
+    // // accessing custom observer
+    // customInterval.subscribe((value) => console.log(value),
+    //                         (error) => console.log(error),
+    //                      () => console.log('completed'));
   }
 
   /**
