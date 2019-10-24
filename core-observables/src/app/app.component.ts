921bb2da68cf299b17cd60a689bfdc2ef97ee013
Tansu Dasli <tansudasli@gmail.com>
2019-10-24 11:51:12 +0300

    Standard rxjs/ interval() and custom impl. w/ rxjs/Observable

--- core-observables/src/app/app.component.ts
+++ core-observables/src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Observable, Observer, Subscription} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // interval(1000)
+    //   .pipe(take(50))
+    //   .subscribe((value) => {
+    //     console.log(value);
+    //   });
+
+    // standard observer
+    // this.subscription = interval(1000).subscribe((value) => { console.log(value); },
+    //                                                    (error) => { console.log(error); });
+
+    // custom observer of interval
+    const customInterval = new Observable((observer) => {
+      let counter = 0;
+      setInterval(() => { observer.next(counter++); }, 1000);
+    });
+
+    customInterval.subscribe((value) => console.log(value));
+  }
+
+  /**
+   * It is mandatory only for Observables except angular provided ones such as this.route.params
+   */
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+
+    // this.subscription.unsubscribe();
+  }
 }
