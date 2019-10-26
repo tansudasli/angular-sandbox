acb2818320cb06bb9d84bfd76b03e4f15568b3ee
Tansu Dasli <tansudasli@gmail.com>
2019-10-26 16:22:58 +0300

    Template-driven - accessing form data, resetting

--- core-forms-template/src/app/app.component.ts
+++ core-forms-template/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,6 +7,8 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) form: NgForm;
+
   defaultSecret = 'pet';
   secretAnswer: string;
 
@@ -15,10 +17,37 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // the other way is using 1-way binding, but below approach is better! patchValue is the best :)
+
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   secretAnswer: 'n/a',
+    //   gender: 'female'
+    // });
+
+    this.form.form.patchValue({
+        userData: {
+        username: suggestedName
+      },
+    });
   }
 
-  onSubmit(form: NgForm) {
-    console.log(`onSubmit event emmited ${form}`);
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   console.log(`onSubmit event emmited ${form}`);
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(`onSubmit event emmited ${this.form}`);
+    console.log(this.form);
+  }
+
+  onReset() {
+    this.form.reset();
   }
 }
