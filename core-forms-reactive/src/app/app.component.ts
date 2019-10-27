6b5d82ac022379167125d3ea8d012f038ec43216
Tansu Dasli <tansudasli@gmail.com>
2019-10-27 17:19:36 +0300

    Reactive-driven - form creation

--- core-forms-reactive/src/app/app.component.ts
+++ core-forms-reactive/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
   signupForm: FormGroup;
 
   genders = ['male', 'female'];
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+
+    });
+  }
 
 }
