16a7b2696e192e0d37bf0476554a2994ddace9a7
Tansu Dasli <tansudasli@gmail.com>
2019-10-27 17:44:14 +0300

    Reactive-driven - validations

--- core-forms-reactive/src/app/app.component.ts
+++ core-forms-reactive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +13,15 @@ export class AppComponent implements  OnInit {
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male'),
 
     });
   }
 
+  onSubmit() {
+    // same form group in template-driven. controls, value etc..
+    console.log(this.signupForm);
+  }
 }
